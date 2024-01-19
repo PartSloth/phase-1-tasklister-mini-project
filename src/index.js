17e31ed7fdd0a373f5458b9b73daf35e0324dd96
Louiza Mak <louiza.mak@gmail.com>
2024-01-19 16:29:18 -0600

    Complete lab and stretch deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  let priorityValue = document.querySelector('select').value;
+  let userName = document.querySelector('#user_name').value;
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'X';
+  p.textContent = `${userName}: ${todo} `;
+  p.style.color = getColorFromValue(priorityValue);
+  p.appendChild(btn);
+  console.log(priorityValue);
+  document.getElementById(priorityValue).appendChild(p).contentEditable = "true";
+};
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
+function getColorFromValue(priorityValue) {
+  let color = "";
+  switch (priorityValue) {
+    case "1":
+      color = "red";
+      break;
+    case "2":
+      color = "yellow";
+      break;
+    case "3":
+      color = "green";
+      break;
+  }
+  return color;
+}
